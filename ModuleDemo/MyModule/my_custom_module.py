30423357efd400b25b7cf61f0da422589df6daa4
Alisha Maharjan <maharjan.elsa01@gmail.com>
2023-08-23 17:03:24 +0545

    Custom and Inbulit Module Added

---
+++ ModuleDemo/MyModule/my_custom_module.py
@@ -0,0 +1,5 @@
+def func1():
+    print("Func1")
+
+def func2():
+    print("Func2")
---
+++ ModuleDemo/test_module.py
@@ -0,0 +1,8 @@
+from MyModule.my_custom_module import func1
+from MyModule.my_custom_module import func2
+
+from math import factorial
+
+func1()
+func2()
+print(factorial(8))
